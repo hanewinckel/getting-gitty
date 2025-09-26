e69766291abc7cd6d5d3b6cdc6bc8360e3e59c73
Nick Hanewinckel <nicholas.hanewinckel@partnerre.com>
2025-09-26 13:37:33 -0400

    added interest function

--- R/interest.R
+++ R/interest.R
@@ -1,8 +1,9 @@
 
 #' Title
 #'
-#' @param i_nominal_monthly - numeric annual nominal rate as a decimal
-#' 
+#' @param i_nominal numeric nominal interest rate
+#' @param n integer compounding periods
+#'
 #' @description 
 #' Returns annual rate from monthly nominal
 #'
@@ -10,9 +11,13 @@
 #' @export
 #'
 #' @examples
-get_apr <- function(i_nominal_monthly){
-  checkmate::assert_numeric(i_nominal_monthly, any.missing = FALSE, lower = 0)
-  periodic_rate <- i_nominal_monthly / 12
-  apr <- (1 + periodic_rate) ^ 12 - 1
-  return(apr)
+get_apr <- function(i_nominal, n = 12){
+  checkmate::assert_numeric(i_nominal, any.missing = FALSE, lower = 0)
+  checkmate::assert_numeric(n, any.missing = FALSE)
+  if (is.infinite(n)) {return(exp(i_nominal) - 1)}
+  if (!is.infinite(n)){
+    periodic_rate <- i_nominal / n
+    apr <- (1 + periodic_rate) ^ n - 1
+    return(apr)
+  }
 }
